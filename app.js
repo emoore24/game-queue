7df7108134e74c021b10d38cf2254cb166733b06
emoore24 <evan.moore24@gmail.com>
2016-06-22 00:04:07 -0700

    add rendering engine, skeleton files, flesh out app.js

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+
+var roomCtrl = require('./controllers/room');
+
 var app = express();
 
-app.get('/', function(req, res) {
-   res.send('Hello World!'); 
+// view engine setup
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
 });
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+   res.render('index');
+});
+
+app.get('/room/:roomId', roomCtrl.getRoom);
 
 
 app.listen(3000, function() {
